43f51621b31bcd24bc6a7e878aabb66a4ce0fe2b
woocheol <newell729@naver.com>
2015-11-16 22:16:30 +0900

    CollisionManager 일부 작업

--- CollisionManager.h
+++ CollisionManager.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <d3dx9.h>
+
+enum Result{ NONE = 0, ONEPOINT = 1, TWOPOINT = 2};
+
+typedef Result E_RESULT;
+
 class CCollisionManager
 {
 public:
+	E_RESULT PointToPoint(D3DXVECTOR3* a_vPt1, D3DXVECTOR3* a_vPt2);
+	E_RESULT PointToLine();
+	E_RESULT PointToPlane();
+	E_RESULT PointToRect();
+	E_RESULT RectToRect();
+	E_RESULT SphereToSphere();
+	E_RESULT SphereToLine();
+
+
 	CCollisionManager();
 	~CCollisionManager();
 };
